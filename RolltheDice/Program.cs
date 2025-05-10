abced9bd5cee10805125c5198f5fe0b6613a05c2
nomz <omnes517@gmail.com>
2025-05-10 15:38:11 -0700

    change 2

---
+++ RolltheDice/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Roll the Dice! A dice rollin application");
+Console.WriteLine("\n The dice has 6 sides");
+Console.WriteLine("\n Press any key to roll the die. Good luck!");
+// Console.ReadKey();
+
+ConsoleKey keyPressed = Console.ReadKey().Key;
+while (keyPressed != ConsoleKey.Escape)
+{
+    Console.Clear();
+    int randomDiceRoll = 0;
+    Random random = new Random();
+    randomDiceRoll = random.Next(1, 7);
+
+    Console.WriteLine("You rolled a " + randomDiceRoll);
+    keyPressed = Console.ReadKey().Key;
+}
+
