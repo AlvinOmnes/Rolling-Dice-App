a966be4c0090cab497e1725d656a50a43b5beee6
nomz <omnes517@gmail.com>
2025-05-10 16:51:14 -0700

    simple calculator program

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Simple Calculator");
+Console.WriteLine("-----------------");
+
+Console.Write("Enter first number: ");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter an operator (+, -):");
+string op = Console.ReadLine();
+
+double result;
+
+switch (op)
+{
+    case "+":
+        result = num1 + num2;
+        break;
+    case "-":
+        result = num1 - num2;
+        break;
+    default:
+        Console.WriteLine("Invalid Operator");
+        return;
+}
+
+Console.WriteLine("Result: " + result);
